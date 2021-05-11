7c7a83e14ad2bbdc751c66c8ef06ab59eefc00b4
Rupal <rupalmalik620@gmail.com>
2021-05-11 14:31:58 +0530

    bakery page

--- dailykit/src/components/Bakeries.js
+++ dailykit/src/components/Bakeries.js
@@ -8,6 +8,7 @@ export class Bakeries extends Component {
                     <h2 className="green_heading">Bakers can get the most out ofMeal Kits by enjoying</h2>
                     <h1 className="black_heading">Substantial Revenue, Diverse menu & Increased Customer Loyalty</h1>
                 </div>
+                {/* <--first sidebar--> */}
                 <div className="container">
                     <div className="row">
                         <div className="col-3" > <h3 className="sidebar_heading">WHY SHOULD BAKERIES DO MEAL KITS?</h3>
@@ -19,6 +20,7 @@ export class Bakeries extends Component {
                                 <li><a href="#Higher Profit Margin" className="sidebar_link">Higher Profit Margin</a></li>
                             </ul>
                         </div>
+                         {/* <--first Para--> */}
                         <div className="col-6"><p className="para_first">
                             <span className="para_heading">Who doesn’t love baking? </span>
                             Having the sweet aroma of freshly baked delicacies fill up your home. While you get the dreamy picture, the reality is, for 90% of people, this sweet aroma is a dream.
@@ -52,27 +54,66 @@ export class Bakeries extends Component {
                     </div>
                 </div>
 
+
+                {/* <--first sidebar--> */}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3" > <h3 className="sidebar_heading">WHY SHOULD BAKERIES DO MEAL KITS?</h3>
+                            <ul className="link_container">
+                                <li><a href="#Recipes" className="sidebar_link">Recipes</a></li>
+                                <li><a href="#Manufacturing/Fulfillment" className="sidebar_link">Manufacturing/Fulfillment</a></li>
+                                <li><a href="#Market and Sell" className="sidebar_link">Market and Sell</a></li>
+                                <li><a href="#Delivery" className="sidebar_link">Delivery</a></li>
+                            </ul>
+                        </div>
+                         {/* <--first Para--> */}
+                        <div className="col-6"><p className="para_first">
+                            <span className="para_heading">We hope </span>
+                            after reading “why bakeries should add meal kits”, you’re eager and relaxed to know that with dailykit platform, this will be a piece of cake.
+                            <br /><br />
+                            Following are the 4 ways dailykit helps you with your meal kit business
+                            <br /><br />
+                            <h4 className="para2_heading" id="Recipes">Recipes</h4>
+                            Using DailyKit recipe tools, you’d be able to curate your recipes to be sold as meal kits. We’ll keep you updated on all the latest trends in recipes so you stay on top of industry.
+                            <br /><br />
+                            <h4 className="para2_heading" id="Manufacturing/Fulfillment">Manufacturing/Fulfillment</h4>
+                            Dailykit solution can be very easily integrated inside your kitchen. Our simple and easy to use weighing scales will make portioning and packing so easy that even a child can pack meal kits (not that you should use child labour). The essence is that dailykit makes packing meal kits idiot proof, leaving no room for errors.
+                            <br/><br/>
+                            <h4 className="para2_heading" id="Market and Sell">Market and Sell</h4>
+                            Dailykit’s omni channel commerce solution allows you to plan your inventory to keep on shelf and use POS for in-store customers. We also help you set up your website for free. You can choose to sell on marketplaces using easy integrations.
+                            <br /><br />
+                            We’re coming soon with vending machine that can be loaded up with pre-packaged ingredients that fall at once to form a meal kit. We’ll keep you posted with this.
+                            <br /><br />
+                            <h4 className="para2_heading" id="Delivery">Delivery</h4>
+                            The advantage with bakery products being shelf stable is that you can choose to extend your delivery area using delivery partners, both hyperlocal and nationwide shipping companies.
+                            <br /><br />
+                            Dailykit’s platform is specifically built with keeping the bakery operations in mind and starting with meal kit is super easy. So, with that said, what are you waiting for? Bring us today, our baking kit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <h3 className="Blog_heading">CHECK OUT OUR BLOG</h3>
                 <div className="container">
                     <div className="row">
                         <div className="col">
-                            <div className="card" style={{"width": "18rem"}}>
+                            <div className="card" style={{"width": "28rem","border":" 2px solid black"}}>
                                 <img src="https://www.dailykit.org/hubfs/Screenshot%202021-04-14%20at%206.55.19%20PM.png" className="card-img-top" alt="..."/>
                                     <div className="card-body">
                                         <h5 className="card-title">HOW TO LAUNCH MEAL KITS AT YOUR RESTAURANT?</h5>
                                         <small> by Roshan D Souza posted at 4/14/21 12:00 PM</small>
                                         <p className="card-text">There is an urgent need to make sure Restaurants try every possible way in which they can get people ordering. If you think you as a Restaurateur have tried everything then, think again.  Yes,...</p>
-                                        <a href="#" className="">Read more</a>
+                                        <a href="#Readmore" className="ReadMore">Read more</a>
                                     </div>
                             </div>
                         </div>
                             <div className="col">
-                                <div className="card" style={{"width": "18rem"}}>
+                                <div className="card" style={{"width": "28rem","border":" 2px solid black"}}>
                                     <img src="https://lh6.googleusercontent.com/W179jGz7_CWkKJJHsQeertViy_N7W6nQASgk07vzJ_m6WWSfKCO3-BZzi6yAdjzak2R8o2758sHE8ovCYo9cWXrEZJMFK3KEXIttymyJ1FiqNIPQgQ0azI0R8OMoZeAiZzOqe1_A" className="card-img-top" alt="..."/>
                                         <div className="card-body">
                                             <h5 className="card-title">YOUR PACKAGING SHOULD BUILD BRAND RECALL</h5>
                                             <small> by Ananya Rajput posted at 4/2/21 12:00 PM</small>
                                             <p className="card-text">Your brand is in a constant battle to retain customers, build confidence and encourage brand recall. To ensure this, a brand needs to build their brand voice. When it comes to food, brand voice...</p>
-                                            <a href="#" className="">Read more</a>
+                                            <a href="#Readmore" className="ReadMore">Read more</a>
                                         </div>
                                 </div>
                             </div>

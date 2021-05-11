12177736930af3ca48417206f929fce2c98432f8
Rupal <rupalmalik620@gmail.com>
2021-05-11 18:08:34 +0530

    grocery

--- dailykit/src/Footer.js
+++ dailykit/src/Footer.js
@@ -1,11 +1,47 @@
-import React, { PureComponent } from 'react'
+import React from "react";
+import dailykit_logo from "./dailykit_logo.svg";
+import "./style.css";
+const Footer = () => {
+  return (
+    <>
+      <div class="flex-container">
+              <div>
+                  <img src={dailykit_logo} style={{"width":"50px","paddingBottom":"1rem"}} alt="footer_logo"/>
+          <p>
+            DailyKit is a Meal Kit Technology Platform
+            <br />
+            Our software suite - recipe management, manufacturing solution,
+            sales and marketing platforms and delivery integrations are
+            everything that your company needs to grow.
+          </p>
+        </div>
+              <div className="footer_section">
+          <h3>ABOUT US</h3>
+          <p>DailyKit Philosophy</p>
+        </div>
+        <div className="footer_section"><ul>
+          <h3>START A MEAL KIT BRAND</h3>
+          
+            <li>QSR & Cafes</li>
+            <li>Casual and Fine Dining</li>
+            <li>Cloud Kitchen</li>
+            <li>Bakery</li>
+          </ul>
+        </div>
+        <div className="footer_section"> <ul>
+          <h3>Contact us</h3>
+         
+            <li>Rishi@dailykit.org</li>
 
-export default class Footer extends PureComponent {
-    render() {
-        return (
-            <div>
-                Footer
-            </div>
-        )
-    }
-}
+            <li>+13125210704</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="copyright">
+        Copyright © 2021 DailyKit Inc. All Rights Reserved.
+      </div>
+    </>
+  );
+};
+export default Footer;

1fe04745b47926bf652e5b4890ddfafde9bf1d44
Rupal <rupalmalik620@gmail.com>
2021-05-27 14:53:16 +0530

    fix : specialBanner3 heading

--- src/components/orderpage/SpecialBanner3.jsx
+++ src/components/orderpage/SpecialBanner3.jsx
@@ -6,11 +6,18 @@ export default class SpecialBanner3 extends PureComponent {
   render() {
     return (
       <div style={{ marginTop: "6rem" }}>
-     {this.props.SubHeading2text &&   <SubHeading1 text={this.props.SubHeading2text} />}
+        <div
+          class="container"
+          style={{
+            padding: "2rem 0rem 6rem 0rem",
+          }}
+        >
+  {this.props.SubHeading2text &&   <SubHeading1 text={this.props.SubHeading2text} />}
         {this.props.Heading1text && <Heading1 text={this.props.Heading1text} />}
         {this.props.SubHeading1text && <SubHeading1 text={this.props.SubHeading1text} />}
         
         <div class="row">
+        
           <div class="col-md-6 col-xs-10 ">
             <img
               src="https://www.dailykit.org/hubfs/dailykit-assets/order-fulfilment/consumer@2x.png"
@@ -26,7 +33,8 @@ export default class SpecialBanner3 extends PureComponent {
             />
           </div>
         </div>
-      </div>
+        </div>
+        </div>
     );
   }
 }
